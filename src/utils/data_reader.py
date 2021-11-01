960eed6db3ebdee64e2fd6c10a6e45dec4b5ed28
Steven HH Chen <s335276@gmail.com>
2021-11-02 02:20:04 +0800

    feat(utils/data_reader): add IBM QSDG data reading function

--- src/utils/data_reader.py
+++ src/utils/data_reader.py
@@ -19,3 +19,23 @@ def read_gmb_data() -> List[List[str]]:
             assert isinstance(item, str)
 
     return gmb_list_data
+
+
+def read_qsdg_data() -> List[List[str]]:
+    """Read IBM QSDG Dataset
+
+    Returns:
+        List[List[str]]: List of Transactions. For each transaction, it includes items inside.
+    """
+    # TODO: Replace Testing File Name for verification
+    with open('./dataset/IBM/ibm-2021_preprocessed.csv', 'r', encoding='utf-8') as qsdg_list_fd:
+        qsdg_list_data = [line[:-1].split(',') for line in qsdg_list_fd.readlines()]
+    logger.debug(f"Read {len(qsdg_list_data)} Transactions")
+
+    # Verify Data Type
+    for transaction in qsdg_list_data:
+        assert isinstance(transaction, list)
+        for item in transaction:
+            assert isinstance(item, str)
+
+    return qsdg_list_data
